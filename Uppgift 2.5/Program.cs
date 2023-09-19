aa7c382fdc3f15089c6183d6c7df5ae99a3fab5b
hast06001 <hast06001@E-PW024NW1>
2023-09-19 21:35:55 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_2._5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in en beräkning med två decimaltal där talen multipliceras med varandra. (ex. 5,2*3,8)");
+            string text = Console.ReadLine();
+            string Texttal1 = text[..2];
+            string Texttal2 = text[4..];
+            float tal1 = float.Parse(Texttal1);
+            float tal2 = float.Parse(Texttal2);
+            float produkt = tal1 * tal2;
+            Console.WriteLine(produkt);
+
+            Console.ReadKey();
+        }
+    }
+}
